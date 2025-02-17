ade95acf83c0a3a8b414ce47fd38d12e6e254c04
charlottelai <charlottelai@student.unsw.edu.au>
2025-02-17 20:16:03 +1100

    added the audio function

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const inputBox = document.getElementById("input-box")
-const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+let player; // YouTube player instance
+
+// Load YouTube API
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '0',
+    width: '0',
+    videoId: 'aTiOiMuSznE', // Your YouTube video ID
+    playerVars: { 'autoplay': 0, 'controls': 0, 'showinfo': 0, 'rel': 0 },
+  });
+}
+
+function playCelebrationSound() {
+  if (player) {
+    player.playVideo();
+    setTimeout(() => {
+      player.pauseVideo();
+    }, 3000); // Stops video after 3 seconds
+  }
+}
 
 function addTask() {
   if (inputBox.value === '') {
     alert("You must write something!");
-  }
-  else {
+  } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
@@ -18,22 +38,26 @@ function addTask() {
   saveData();
 }
 
-listContainer.addEventListener("click", function(e){
+listContainer.addEventListener("click", function(e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+
+    if (e.target.classList.contains("checked")) {
+      playCelebrationSound(); // Play YouTube video when checked
+    }
+
     saveData();
-  }
-  else if(e.target.tagName === "SPAN"){
+  } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
     saveData();
   }
 }, false);
 
-function saveData(){
+function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 
